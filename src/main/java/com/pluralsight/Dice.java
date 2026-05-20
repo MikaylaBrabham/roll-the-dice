192f47b86a00e5912480ea8b4b1f2038c4795ae1
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-20 02:02:05 -0500

    added main , for, roll count

--- src/main/java/com/pluralsight/Dice.java
+++ src/main/java/com/pluralsight/Dice.java
@@ -1,19 +1,41 @@
 package com.pluralsight;
+
 import java.math.*;
+
 public class Dice {
+
+
+
 
     //add roll
     public int roll() {
-        // 2,4,6,7 roll count
-        int two = 0;
-        int four = 0;
-        int six = 0;
-        int seven = 0;
 
+    //add random
+    int randomNumber = (int)(Math.random() * maxValue) + minValue;
 
-            //add random
-            int randomNumber = (int) (Math.random() * maxValue) + minValue;
-
-
-        }
     }
+
+    public static void main(String[] args) {
+
+        //add dice
+        Dice dice = new Dice();
+
+
+        //add for
+        for ( int i = 1; i <= 100; i++) {
+
+            //add int variables
+            int roll1 = dice.roll();
+            int roll2 = dice.roll();
+
+
+            // 2,4,6,7 roll count
+            int two = 0;
+            int four = 0;
+            int six = 0;
+            int seven = 0;
+        }
+
+
+    }
+}
