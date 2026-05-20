27019a9b04908d4d56ea4a5b1ccb5356f6ad2897
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-20 01:49:35 -0500

    added roll and the count

---
+++ src/main/java/com/pluralsight/Dice.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+import java.math.*;
+public class Dice {
+
+    //add roll
+    public int roll() {
+        // 2,4,6,7 roll count
+        int two = 0;
+        int four = 0;
+        int six = 0;
+        int seven = 0;
+
+
+            //add random
+            int randomNumber = (int) (Math.random() * maxValue) + minValue;
+
+
+        }
+    }
--- src/main/java/com/pluralsight/RollTheDice.java
+++ src/main/java/com/pluralsight/RollTheDice.java
@@ -1,4 +1,7 @@
 package com.pluralsight;
 
 public class RollTheDice {
+    public static void main(String[] args) {
+
+    }
 }
